c4b5568c13d9cb557442b5cf29372e2ca458307d
Ellen Brandt Sahlberg <el2610br-s@student.lu.se>
2024-10-10 15:40:41 +0200

    rename var2, var3, var4, var5, var8

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
 
-int main(int arc_counter, char *arc_list[]) {
-  if (arc_counter == 3) {
-    std::string var2{arc_list[0]};
-    auto var3{*(arc_list[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(arc_list[2])};
+int main(int arg_counter, char *arg_list[]) {
+  if (arg_counter== 3) {
+    std::string output_name{arg_list[0]};
+    auto Caracter1_arg_1{*(arg_list[1])};
+    auto Length_output_name{output_name.size()};
+    auto Int_numb_arg_2{std::atoi(arg_list[2])};
     auto var6{0};
     auto var7{0};
-    std::string var8{arc_list[1]};
+    std::string arg_1{arg_list[1]};
     while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
+      var6 += arg_1[var7++];
+      if (var7 >= static_cast<int>(arg_1.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+    if ((var6 ^ Caracter1_arg_1 * 3) <<  Length_output_name & 0x1f) == Int_numb_arg_2 ) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
